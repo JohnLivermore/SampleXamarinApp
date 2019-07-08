cbbbf883ec9dc095e214f506a288c458b928c8da
John Livermore <jlivermore@bigwavesoftware.com>
2019-07-08 09:49:50 -0500

    2 dialog example

--- SampleApp/SampleApp/MainPageModel.cs
+++ SampleApp/SampleApp/MainPageModel.cs
@@ -2,10 +2,30 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
 
 namespace SampleApp
 {
     public class MainPageModel : FreshBasePageModel
     {
+        public bool Dialog1Open { get; set; }
+        public bool Dialog2Open { get; set; }
+
+        public ICommand OpenDialog1
+        {
+            get
+            {
+                return new Command(() => { Dialog1Open = true; });
+            }
+        }
+
+        public ICommand OpenDialog2
+        {
+            get
+            {
+                return new Command(() => { Dialog2Open = true; });
+            }
+        }
     }
 }
