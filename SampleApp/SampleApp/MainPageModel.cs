981ef94a31da32f1eaa7dcfa1ac840582b2a8ad8
John Livermore <jlivermore@bigwavesoftware.com>
2019-07-12 17:26:05 -0500

    update

--- SampleApp/SampleApp/MainPageModel.cs
+++ SampleApp/SampleApp/MainPageModel.cs
@@ -1,4 +1,5 @@
 ﻿using FreshMvvm;
+using PropertyChanged;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -7,5 +8,22 @@ namespace SampleApp
 {
     public class MainPageModel : FreshBasePageModel
     {
+        public MainPageModel()
+        {
+            Items = new List<ViewModel>() {
+                new ViewModel(){Name="A"},
+                new ViewModel(){Name="B"},
+                new ViewModel(){Name="C"},
+                new ViewModel(){Name="D"}
+            };
+        }
+
+        public List<ViewModel> Items { get; set; }
+    }
+
+    [AddINotifyPropertyChangedInterface]
+    public class ViewModel
+    {
+        public string Name { get; set; }
     }
 }
