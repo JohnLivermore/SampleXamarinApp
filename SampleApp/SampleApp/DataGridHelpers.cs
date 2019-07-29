174a37960752147332d4fbb25f17bb8678160870
John Livermore <jlivermore@bigwavesoftware.com>
2019-07-29 16:02:56 -0500

    code shows issue now

---
+++ SampleApp/SampleApp/DataGridHelpers.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Telerik.XamarinForms.DataGrid;
+using Xamarin.Forms;
+
+namespace SampleApp
+{
+    public static class DataGridHelpers
+    {
+        public static DataGridColumnHeaderStyle NoOptionsHeaderStyle
+        {
+            get
+            {
+                // Always return a new instance so that you avoid issues with multiple columns using the same instance.
+                var colHeaderStyle = new DataGridColumnHeaderStyle();
+
+                colHeaderStyle.OptionsButtonTextColor = Color.Transparent;
+                colHeaderStyle.OptionsButtonFontSize = 0;
+                colHeaderStyle.BorderColor = new Color(0xd9, 0xd9, 0xd9);
+                colHeaderStyle.BorderThickness = 1;
+
+                return colHeaderStyle;
+            }
+        }
+    }
+}
--- SampleApp/SampleApp/MainPageModel.cs
+++ SampleApp/SampleApp/MainPageModel.cs
@@ -1,11 +1,25 @@
 ﻿using FreshMvvm;
+using PropertyChanged;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Text;
 
 namespace SampleApp
 {
     public class MainPageModel : FreshBasePageModel
     {
+        public MainPageModel()
+        {
+            Source = new ObservableCollection<Entity>();
+        }
+
+        public ObservableCollection<Entity> Source { get; set; }
+
+    }
+
+    public class Entity
+    {
+        public string Name { get; set; }
     }
 }
