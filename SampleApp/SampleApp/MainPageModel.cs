59922086710cd218e325b577cd80d662e0b5ed9c
John Livermore <jlivermore@bigwavesoftware.com>
2019-09-12 16:07:34 -0500

    1

--- SampleApp/SampleApp/MainPageModel.cs
+++ SampleApp/SampleApp/MainPageModel.cs
@@ -2,10 +2,41 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using Xamarin.Forms;
 
 namespace SampleApp
 {
     public class MainPageModel : FreshBasePageModel
     {
+        public MainPageModel() : base()
+        {
+            GaugeSites = new List<GaugeSite>();
+
+            for (var index = 1; index <= 5; index++)
+            {
+                GaugeSites.Add(new GaugeSite()
+                {
+                    Description = $"Gauge Site {index}"
+                });
+            }
+        }
+
+        public List<GaugeSite> GaugeSites { get; set; }
+
+        public Command DoSomethingInteresting
+        {
+            get
+            {
+                return new Command(() =>
+                {
+
+                });
+            }
+        }
+    }
+
+    public class GaugeSite
+    {
+        public string Description { get; set; }
     }
 }
