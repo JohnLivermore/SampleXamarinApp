1335dfeb5c8c5375dee0331bd52a16dc93602ca6
John Livermore <jlivermore@bigwavesoftware.com>
2019-10-10 16:27:35 -0500

    1

--- SampleApp/SampleApp/App.xaml.cs
+++ SampleApp/SampleApp/App.xaml.cs
@@ -15,20 +15,5 @@ namespace SampleApp
 
             MainPage = nav;
         }
-
-        protected override void OnStart()
-        {
-            // Handle when your app starts
-        }
-
-        protected override void OnSleep()
-        {
-            // Handle when your app sleeps
-        }
-
-        protected override void OnResume()
-        {
-            // Handle when your app resumes
-        }
     }
 }
