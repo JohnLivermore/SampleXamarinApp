38e243667698a192f9672161c4cbb2043b52367d
John Livermore <jlivermore@bigwavesoftware.com>
2019-10-17 15:34:55 -0500

    1

--- SampleApp/SampleApp/MainPageModel.cs
+++ SampleApp/SampleApp/MainPageModel.cs
@@ -2,10 +2,23 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using Xamarin.Forms;
 
 namespace SampleApp
 {
     public class MainPageModel : FreshBasePageModel
     {
+        // template command
+        public Command OpenDialog
+        {
+            get
+            {
+                return new Command(_ =>
+                {
+                    CoreMethods.PushPageModel<DialogPageModel>(null, modal: true);
+                });
+            }
+        }
+
     }
 }
