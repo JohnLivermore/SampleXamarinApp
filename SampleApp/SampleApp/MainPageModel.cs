07e0a47c9b5162eab33215ad8cacd404ccb4a6a2
John Livermore <jlivermore@bigwavesoftware.com>
2020-01-17 08:00:08 -0600

    swipeview error

--- SampleApp/SampleApp/MainPageModel.cs
+++ SampleApp/SampleApp/MainPageModel.cs
@@ -2,10 +2,31 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using Xamarin.Forms;
 
 namespace SampleApp
 {
     public class MainPageModel : FreshBasePageModel
     {
+        public MainPageModel()
+        {
+            Items = new List<string>();
+
+            Items.Add("Apple");
+            Items.Add("Banana");
+            Items.Add("Pear");
+        }
+
+        public Command Delete
+        {
+            get
+            {
+                return new Command(_ =>
+                {
+                });
+            }
+        }
+
+        public List<string> Items { get; set; }
     }
 }
