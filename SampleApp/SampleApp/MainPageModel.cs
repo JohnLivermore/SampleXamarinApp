22afab56a11619aade0181757b3cd5a7cdaa76cc
John Livermore <jlivermore@bigwavesoftware.com>
2020-01-30 17:26:47 -0600

    1

--- SampleApp/SampleApp/MainPageModel.cs
+++ SampleApp/SampleApp/MainPageModel.cs
@@ -1,12 +1,61 @@
-﻿using FreshMvvm;
-using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using FreshMvvm;
+using PropertyChanged;
+using Xamarin.Forms;
 
 namespace SampleApp
 {
-    public class MainPageModel : FreshBasePageModel
+    public class MainPageModel : INotifyPropertyChanged
     {
-        public int? Length { get; set; }
+        public MainPageModel()
+        {
+            LengthString = "";
+        }
+
+        private int? _length;
+        public string _lengthString;
+
+        public int? Length
+        {
+            get { return _length; }
+            set { SetProperty(ref _length, value); }
+        }
+        public string LengthString
+        {
+            get { return _lengthString; }
+            set { SetProperty(ref _lengthString, value); }
+        }
+
+        // template command
+        public Command Process
+        {
+            get
+            {
+                return new Command(_ =>
+                {
+                    LengthString = Length.ToString();
+                });
+            }
+        }
+
+        protected virtual void OnPropertyChanged([CallerMemberName]string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected bool SetProperty<T>(ref T storage, T value, [CallerMemberName]string propertyName = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(storage, value))
+            {
+                return false;
+            }
+            storage = value;
+            OnPropertyChanged(propertyName);
+
+            return true;
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
     }
 }
