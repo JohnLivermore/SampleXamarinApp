5351b93f0494e53320fb1d146df1110325855868
John Livermore <jlivermore@bigwavesoftware.com>
2020-02-07 13:01:12 -0600

    nullDateControl

--- SampleApp/SampleApp/MainPageModel.cs
+++ SampleApp/SampleApp/MainPageModel.cs
@@ -2,12 +2,24 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using Xamarin.Forms;
 
 namespace SampleApp
 {
     public class MainPageModel : FreshBasePageModel
     {
-        public DateTime StartDateTime { get; set; }
         public DateTimeOffset? EndingDateTime { get; set; }
+
+        public Command Check
+        {
+            get
+            {
+                return new Command(_ =>
+                {
+                    var val = EndingDateTime;
+                });
+            }
+        }
+
     }
 }
